261640efe899842c1b117477effe0c87bd847b53
trentwirth <62706609+trentwirth@users.noreply.github.com>
2022-12-18 21:32:46 -0500

    chasing leads

--- textual-tutorial/minutes.py
+++ textual-tutorial/minutes.py
@@ -1,6 +1,7 @@
 from textual.app import App, ComposeResult
 from textual.widgets import TextLog
 from textual.widgets import Input
+from textual.widgets import Static
 from textual import events
 
 import time
@@ -11,23 +12,30 @@ def get_timestamp() -> str:
 
 class InputApp(Input):
 
-    class Minutes(TextLog):
+    # class Minutes(TextLog):
 
-        async def log_minutes(self, s: str|int) -> None:
-            self.write(get_timestamp())
-            self.write(time.time())
-            self.write(s)
+    #     async def log_minutes(self, s: str|int) -> None:
+    #         self.write(get_timestamp())
+    #         self.write(time.time())
+    #         self.write(s)
 
-            print('In log_minutes')
+    #         print('In log_minutes')
+
+    class Debug_Print():
+
+        async def debug_print() -> None:
+            print("debug-001")
 
     async def key_enter(self) -> None:
 
         print(self.value)
 
-        self.emit(self.Minutes.log_minutes(self, self.value))
+        # self.emit(self.Minutes.log_minutes(self, self.value))
 
         # Clear the text input
         self.value = ''
+
+        await self.emit(InputApp.Debug_Print.debug_print())
 
         
     
@@ -39,7 +47,7 @@ class MinutesApp(App):
 
     def compose(self) -> ComposeResult:
         yield InputApp(classes="input_box")
-        yield InputApp.Minutes()
+        # yield InputApp.Minutes()
 
 
 if __name__ == "__main__":
